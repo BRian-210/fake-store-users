f3d02ccfc2dff0d9bf58bff9e0e0df21176e5f95
BRIAN GITHINJI <brian.githinji3@student.moringaschool.com>
2025-08-29 08:50:34 -0700

    git commit -m "Refactor UserCard component to improve readability and performance"

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -5,11 +5,17 @@ import styles from '../styles/UserCard.module.css';
 const UserCard = ({ user }) => {
   const { deleteUser } = useUserStore();
 
+  if (!user || !user.id) {
+    return <div className={styles.card}>No user data available</div>;
+  }
+
   return (
     <div className={styles.card}>
-      <div className={styles.username}>{user.username}</div>
-      <div className={styles.email}>{user.email}</div>
-      <div>{user.name.firstname ? user.name.firstname : ''} {user.name.lastname ? user.name.lastname : ''}</div>
+      <div className={styles.username}>{user.username || 'No username'}</div>
+      <div className={styles.email}>{user.email || 'No email'}</div>
+      <div>
+        {user.name?.firstname || ''} {user.name?.lastname || ''}
+      </div>
       <div className={styles.actions}>
         <Link to={`/user/${user.id}`}>Details</Link>
         <Link to={`/edit/${user.id}`}>Edit</Link>
